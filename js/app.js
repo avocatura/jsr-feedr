a502068f23001be68f6b266ed5906a8135ac7067
avocatura <32111692+avocatura@users.noreply.github.com>
2018-07-10 16:18:20 -0500

    Commit 2

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ $(".source").click(function () {
 $("#feedrButton").click(function() {
 	$("body").toggleClass("loader")
 	callAPI(urlGuardian);
+	$("#clickSource").html("The Guardian");
 })
 
 //Each API url
@@ -39,8 +40,6 @@ var callAPI = function(url) {
 	request.send();
 	request.onload = function() {
 	var x = JSON.parse(request.response);
-	//console.log("data", request.response);
-	console.log(url);
 	//Promise function to toggle body loader class when the API has loaded
 	var promiseApi = new Promise(function(resolve, reject) {
 	if (request.status === 200) {
@@ -57,20 +56,20 @@ var callAPI = function(url) {
 	for (let i = 0; i < articles.length; i++) {
 	//define DOM variables depending on API
 	if (url === urlGuardian) {
-		webTitle = x.response.results[i].webTitle;
-		sectionName = x.response.results[i].sectionName;
-		thumbnail = x.response.results[i].fields.thumbnail
-		impressionsValue = x.response.results[i].fields.wordcount
+		var webTitle = x.response.results[i].webTitle;
+		var sectionName = x.response.results[i].sectionName;
+		var thumbnail = x.response.results[i].fields.thumbnail
+		var impressionsValue = x.response.results[i].fields.wordcount;
 	} else if (url === urlUSA) {
-		webTitle = x.articles[i].title;
-		sectionName = "sectionName";
-		thumbnail = x.articles[i].urlToImage;
-		impressionsValue = x.articles[i].publishedAt.substring(5,10);
+		var webTitle = x.articles[i].title;
+		var sectionName = x.articles[i].source.name;
+		var thumbnail = x.articles[i].urlToImage;
+		var impressionsValue = x.articles[i].publishedAt.substring(5,10);
 	} else {
-		webTitle = x.results[i].title;
-		sectionName = x.results[i].section;
-		thumbnail = x.results[i].multimedia[0].url;
-		impressionsValue = x.results[i].updated_date.substring(5,10);
+		var webTitle = x.results[i].title;
+		var sectionName = x.results[i].section;
+		var thumbnail = x.results[i].multimedia[0].url;
+		var impressionsValue = x.results[i].updated_date.substring(5,10);
 	}
 	//Select DOM elements and replace with API variables
 	var articleTitle = $(articleContent[i]).find("h3")[0]; 
@@ -89,19 +88,17 @@ var callAPI = function(url) {
 		var index = $("h3").index(this);
 		$("#popUp").toggleClass("hidden");
 		$("#popUpTitle").html($(this).html());
-
-		if (url === urlGuardian) {
+		console.log(url);
+		if ($("#clickSource").html() === "The Guardian") {
 			var webUrl = x.response.results[index].webUrl;
-			var description = x.response.results[index].blocks.requestedBodyBlocks;
-			console.log(description);
-		} 	else if (url === urlUSA) {
+			var description = x.response.results[index].blocks.requestedBodyBlocks["body:latest"][0].bodyTextSummary.substring(0,800);
+		} else if ($("#clickSource").html() === "USA Today") {
 			var webUrl = x.articles[index].url;
 			var description = x.articles[index].description; 
-		} else {
+		} else if ($("#clickSource").html() === "The New York Times"){
 			var webUrl = x.results[index].url;
 			var description = x.results[index].abstract;
 		}
-
 		$("#popUp").find('p').html(description);
 		$(".popUpAction").click(function () {
 			$(this).attr("href", webUrl);
